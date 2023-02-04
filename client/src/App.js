9b728031bc196da19b55ab990e3aebcb2f22ea74
Angel <aavila002@citymail.cuny.edu>
2023-02-04 06:11:47 -0500

    Sidebar Template

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Sidebar />
     </div>
   )
 }
