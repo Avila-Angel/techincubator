4639a2a635a831bf563709893fc602e0cb9f39f6
NathanielMention <nathanielmention@gmail.com>
2023-02-04 19:53:28 -0500

    fix main page

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
 import Sidebar from "../Sidebar/Sidebar";
+import Createpost from "../CreatePost/CreatePost";
 
 function Main() {
   return (
-    <div>
-      <Navbar></Navbar>
-      <Sidebar></Sidebar>
+    <div className="app-container">
+      <Navbar />
+      <div className="main-container">
+        <Sidebar />
+        <Createpost />
+      </div>
     </div>
   );
 }
